ac832a54c5395f0a224540fe032c34daa1110fc0
meguia <camilo@maclapso.local>
2019-03-29 15:50:11 -0300

    actualizacion del daemon

--- i2call.sh
+++ i2call.sh
@@ -4,3 +4,4 @@ ssh pi@192.168.0.12 -t /usr/sbin/i2cdetect -y 1 | head -2 | tail -1
 ssh pi@192.168.0.13 -t /usr/sbin/i2cdetect -y 1 | head -2 | tail -1
 ssh pi@192.168.0.14 -t /usr/sbin/i2cdetect -y 1 | head -2 | tail -1
 ssh pi@192.168.0.15 -t /usr/sbin/i2cdetect -y 1 | head -2 | tail -1
+ssh pi@192.168.0.12 -t /usr/sbin/i2cdetect -y 1 | head -2 | tail -1
---
+++ numpytest.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x = np.zeros((3,3),dtype=np.int32)
+
+z = x*2
+
+y = z - x
